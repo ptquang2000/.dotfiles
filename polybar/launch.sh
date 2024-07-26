336a00d4cbcb300bd2db983dbe2bca7ebc7a8b0d
quang.phan <ptquang2000@gmail.com>
2024-07-26 07:47:38 +0700

    use firamono nerd font

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,7 +8,7 @@ polybar-msg cmd quit
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar_dock.log /tmp/polybar_status.log
-polybar dock 2>&1 | tee -a /tmp/polybar_dock.log & disown
-polybar status 2>&1 | tee -a /tmp/polybar_status.log & disown
+polybar -r dock 2>&1 | tee -a /tmp/polybar_dock.log & disown
+polybar -r status 2>&1 | tee -a /tmp/polybar_status.log & disown
 
 echo "Bars launched..."
