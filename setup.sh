23aedfa1d4094645bfbbc6ad1048784a610e4e5b
quang.phantri <ptquang2000@gmail.com>
2025-02-28 07:14:48 +0700

    upd setup

--- setup.sh
+++ setup.sh
@@ -36,6 +36,9 @@ ln -sf $(pwd)/.local/bin ${local_path}/bin
 echo Setting default shell to zsh
 chsh -s /usr/bin/zsh
 
+echo Setting default apps
+xdg-mime default org.pwmt.zathura.desktop application/pdf
+
 echo Setting sddm themes
 git clone https://github.com/stepanzubkov/where-is-my-sddm-theme.git
 cd ./where-is-my-sddm-theme
@@ -52,3 +55,4 @@ echo TODO:
 echo - Setup github SSH key
 echo - Start and Enable reflector.timer
 echo - Enable chrome wayland flags
+echo - Set background image in sddm theme
