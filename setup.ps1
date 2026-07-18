3b64f8e776c23322bc59e41e0aa4faa9a960f7a8
quang.phan <ptquang2000@gmail.com>
2026-07-18 09:56:27 +0700

    upd wins

--- setup.ps1
+++ setup.ps1
@@ -159,8 +159,8 @@ $documents = [Environment]::GetFolderPath('MyDocuments')
 $junctions = @(
     @{ Source = Join-Path $ScriptRoot 'powershell'; Target = Join-Path $documents       'PowerShell' }
     @{ Source = Join-Path $ScriptRoot 'nvim-init';  Target = Join-Path $env:LOCALAPPDATA 'nvim'	}
-    @{ Source = Join-Path $ScriptRoot 'nvim-init/clangd';  Target = Join-Path $env:LOCALAPPDATA 'clangd' }
     @{ Source = Join-Path $ScriptRoot 'psmux';      Target = Join-Path $env:USERPROFILE  '.config\psmux'	}
+    @{ Source = Join-Path $ScriptRoot 'sioyek';     Target = Join-Path $env:LOCALAPPDATA 'sioyek'	}
 )
 foreach ($j in $junctions) {
     New-DotfilesJunction -Source $j.Source -Target $j.Target
